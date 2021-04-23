74dea6d53e8795e9584308c0afe9d5f23f792abb
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2021-04-23 17:57:56 +0200

    if vežbanje

--- 11_IF/script.js
+++ 11_IF/script.js
@@ -8,3 +8,23 @@ if(a < b){
 }
 
 console.log("If je završen");
+
+let x = 15;
+let y = 15;
+if(x == y){
+    console.log("Brojevi x = " + x + " i y = " + y + " su jednaki");
+    console.log(`Brojevi x = ${x} i y = ${y} su jednaki`);
+}
+
+let x1 = "15";
+//==  poredi samo po vrednosti
+if(x == x1){
+    console.log(`Brojevi x i x1 imaju istu vrednost`);
+}
+
+//=== poredi po tipu i po vrednosti
+if(x === x1){
+    console.log(`Brojevi x i x1 imaju isti tip i istu vrednost`);
+    //x i x1 imaju istu vrednost to je 15, međutim nemaju isti tip. x je tima number, dok je x2 tipa string
+}
+
