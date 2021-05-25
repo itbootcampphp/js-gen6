82bab3ea8ec7bbee28002167ab3e2c1086817777
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2021-05-25 17:57:07 +0200

    DOM

--- 20_DOM/script.js
+++ 20_DOM/script.js
@@ -72,3 +72,124 @@ console.log(r9);
 
 let r10 = document.querySelectorAll("#div1"); // querySelectorAll UVEK vraca NodeList, cak i kada se ta lista sastoji od jednog elementa
 console.log(r10);
+
+
+// Menjanje elemenata iz HTML-a
+console.log(r1.id);
+console.log(r1.href);
+console.log(r1.innerHTML);
+//r1.innerHTML = "Sadržaj div elementa je tekst dodat iz JS-a.";
+r1.innerHTML += "Sadržaj div elementa je tekst dodat iz JS-a.";
+
+let prviLink = document.querySelector("a");
+prviLink.href = "http://www.wikipedia.com";
+
+prviLink.setAttribute('target', '_blank');
+//prviLink.target = "_blank";
+prviLink.setAttribute('href', 'http://www.wikipedia.org');
+prviLink.innerHTML = "Wikipedia";
+
+/*
+prviLink.style.color = "red";
+prviLink.style.backgroundColor = "blue";
+prviLink.style.textDecoration = "none";
+//prviLink.style.border = "3px solid black";
+prviLink.style.borderWidth = "3px";
+prviLink.style.borderStyle = "solid";
+prviLink.style.borderColor = "black";
+*/
+
+prviLink.setAttribute('style', 'color: red; background-color: blue; text-decoration: none; border: 3px solid black;');
+
+r1.style.color = "green";
+
+
+// Postaviti crveni border svim linkovima na stranici
+r7.forEach(link => {
+    link.style.border = "3px solid red";
+});
+
+// Drugi nacin
+let linkovi = document.getElementsByTagName("a");
+for(let i = 0; i < linkovi.length; i++) {
+    linkovi[i].style.border = "3px solid green";
+}
+
+//Svim slikama na stranici koja su sa ekstenzijom .png, postaviti okvir na debljinu 2px i crvenu boju. 
+// Prvi nacin
+let slike = document.querySelectorAll("img");
+slike.forEach(slika => {
+    if(slika.src.includes(".png") || slika.src.includes(".PNG")) {
+        slika.style.border = "2px solid red";
+    }
+});
+
+// Drugi nacin
+let slikePng = document.querySelectorAll("img[src*='.png']");
+slikePng.forEach(slika => {
+    slika.style.border = "2px solid red";
+});
+
+// Svakom linku na stranici promeniti target svojstvo 
+linkovi = document.querySelectorAll("a");
+linkovi.forEach(link => {
+    if(link.target === "_blank") {
+        link.target = "_top";
+    }
+    else {
+        link.target = "_blank";
+    }
+});
+
+
+let imena = ["Stefan", "Mladen L.", "Jelena V", "Stefan Dj", "Dusan M"];
+imena.forEach(ime => {
+    if(ime.startsWith("S")) { //if(ime[0] == "S") {
+        document.body.innerHTML += `<a href="#" target="_blank">${ime}</a>`;
+    }
+    else {
+        document.body.innerHTML += `<a href="#">${ime}</a>`;
+    }
+});
+
+
+let result = "<ul>";
+imena.forEach((ime, i) => {
+    if(i % 2 == 0) {
+        result += `<li style="color: blue">${ime}</li>`;
+    }
+    else {
+        result += `<li style="color: red">${ime}</li>`;
+    }
+});
+result += "</ul>";
+document.body.innerHTML += result;
+
+
+
+// Dohvatanje roditeljskog cvora
+
+linkovi = document.querySelectorAll("a");
+
+linkovi.forEach(link => {
+    console.log(link.parentNode);
+    link.parentNode.style.border = "2px solid red";
+});
+
+// Dohvatanje deteta nekog cvora
+let prviDiv = document.querySelector("div.container");
+prviDiv.childNodes.forEach(child => {
+    console.log(child);
+});
+
+let n = prviDiv.childNodes.length;
+for(let i = 0; i < n; i++) {
+    console.log(prviDiv.childNodes[i]);
+}
+
+let link1 = prviDiv.childNodes[1]; 
+console.log(link1.previousSibling);
+console.log(link1.nextSibling);
+console.log(link1.nextSibling.nextSibling);
+
+link1.nextSibling.nextSibling.style.fontSize = "24px";
