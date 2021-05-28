3296b43bd70f3afb644b4d13fd9e993b577c18ff
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2021-05-28 16:30:03 +0200

    21_EVENTS

--- 21_EVENTS/ZADACI/script.js
+++ 21_EVENTS/ZADACI/script.js
@@ -46,3 +46,50 @@ btnMinus.addEventListener('click', () => {
     }
     spanIspis.innerHTML = rez;
 });
+
+// 5. ZADATAK
+// Napraviti input polje i dugme.U input polje treba da se unese ime neke osobe, a na ekranu u paragrafu da se ispiše Zdravo i ime osobe preuzeto iz input polja.
+//DOM
+let btnPosalji = document.getElementById('posalji');
+let inputIme = document.getElementById('ime');
+let pPrikaziIme = document.getElementById('prikaziIme');
+
+btnPosalji.addEventListener('click', () => {
+    let upisanoIme = inputIme.value;
+    pPrikaziIme.innerHTML = `Zdravo ${upisanoIme}`;
+});
+
+// 6. ZADATAK
+// Dopuniti 5. zadatak dodavanjem eventa i proizvoljnog alert-a, kada se klikne dva puta na dugme.
+btnPosalji.addEventListener('dblclick', () => {
+    let upisanoIme = inputIme.value;
+    alert(`Hello ${upisanoIme}`);
+});
+
+// 5. ZADATAK - drugi način
+let inputPrezime = document.getElementById('prezime');
+let btnPosaljiPrezime = document.getElementById('posaljiPrezime');
+let pPrikaziPrezime = document.getElementById('prikaziPrezime');
+
+btnPosaljiPrezime.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e);
+    pPrikaziPrezime.innerHTML = `Uneli ste prezime ${inputPrezime.value}`;
+    inputPrezime.value = "";
+});
+
+// PRIMER
+let forma = document.getElementById('forma');
+let inputJelo = document.getElementById('jelo');
+let inputPice = document.getElementById('pice');
+let pIspis = document.getElementById('ispis');
+
+forma.addEventListener('submit', e => {
+    e.preventDefault();
+    pIspis.innerHTML =
+        `<ul>
+        <li>Omiljeno jelo: ${inputJelo.value}</li>
+        <li>Omiljeno piće: ${inputPice.value} </li>
+    </ul>`;
+    forma.reset(); // resetuje sva polja u formi na koju se odnosi
+});
